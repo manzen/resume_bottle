a8432acf83bacb2dda7513836eb6a6b119feb9c7
iemong <kiyoshidainagon@gmail.com>
2019-11-16 20:25:33 +0900

    Uploaderを作った

---
+++ frontend/components/molecules/Uploader.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react'
+import styled from '@emotion/styled'
+
+const Uploader = () => {
+    const [file, setFile] = React.useState<File | undefined>(undefined)
+    const imageRef = React.useRef<HTMLImageElement | null>(null)
+
+    const handleChange = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0]
+        setFile(file)
+    }, [])
+
+    React.useLayoutEffect(() => {
+        if (!file) return
+        const $img = imageRef.current
+        const reader = new FileReader()
+        reader.onload = evt => {
+            if (!(evt.target && $img)) return
+            $img.src = evt.target.result as string
+        }
+
+        reader.readAsDataURL(file)
+    }, [file])
+
+    return (
+        <Wrapper>
+            {file ? (
+                <PreviewImage ref={imageRef} />
+            ) : (
+                <EmptyBox>
+                    <Label>
+                        <input type="file" onChange={handleChange} />
+                    </Label>
+                    <CloudImage src="/img/cloud_upload.png" alt="" />
+                </EmptyBox>
+            )}
+        </Wrapper>
+    )
+}
+
+export default Uploader
+
+const Wrapper = styled.div``
+
+const EmptyBox = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+    height: 200px;
+    border: 2px dashed #abd6f9;
+    box-sizing: border-box;
+`
+
+const Label = styled.label`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    input {
+        visibility: hidden;
+    }
+`
+
+const CloudImage = styled.img`
+    pointer-events: none;
+`
+
+const PreviewImage = styled.img`
+    max-width: 100%;
+`
--- frontend/components/templates/home/index.tsx
+++ frontend/components/templates/home/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
 import Main from '../layouts/Main'
+import Uploader from '../../molecules/Uploader'
 
 const Home = () => {
     return (
         <Wrapper>
-            <Main>hoge</Main>
+            <Main>
+                <Uploader />
+            </Main>
         </Wrapper>
     )
 }
